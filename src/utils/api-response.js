634bd32286fe52187891a62ea8d15e896c8247b2
Gitika Sharma <gitikasharma8j@gmail.com>
2026-09-04 12:11:00 +0530

    feat: add standardized Response utility

---
+++ src/utils/api-response.js
@@ -0,0 +1,16 @@
+
+class ApiResponse {
+    constructor(
+        statusCode,
+        message,
+        success 
+    ){
+
+        this.statusCode = statusCode,
+        this.message = message,
+        this.success = statusCode < 400
+
+    }
+}
+
+export { ApiResponse }
